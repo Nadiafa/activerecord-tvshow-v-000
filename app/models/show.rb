5d5bc79c8a1ad9dac98e2da8f89bc1ee4b05d435
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-20 13:18:50 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
     # returns the tv show with the highest rating
-    self.select('*')
+    self.select('*').highest_rating
   end 
   
   
