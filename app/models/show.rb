d1ca6ac696d70a9bc70908f91a37388b9d96cd67
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-20 13:27:43 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,15 +21,14 @@ class Show < ActiveRecord::Base
     self.sum(:rating)
   end 
   
-  
   def self.popular_shows
     # returns an array of all of the shows with a rating above 5
     self.select("*").where("rating > 5")
   end 
   
-  
   def self.shows_by_alphabetical_order
     # returns an array of all of the shows, listed in alphabetical order
+    self.select("*").order("name")
   end 
     
     
