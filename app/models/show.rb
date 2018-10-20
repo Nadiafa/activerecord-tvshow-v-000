b69f7c421a5bcf6bd4d5664863018067f98ee4f1
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-20 13:22:37 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    v
+    maximum(:rating)
   end 
   
   def self.most_popular_show
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.lowest_rating
-    minimum(:rating)
+    maximum(:rating)
   end 
   
   
