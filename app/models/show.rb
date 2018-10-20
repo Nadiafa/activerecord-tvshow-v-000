343b694a4dbeb0681fd7a86618b9a831742020a0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-20 12:16:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,36 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end 
   
+  def self.most_popular_show
+    # returns the tv show with the highest rating
+    
+  end 
+  
+  
+  def self.lowest_rating
+    # returns the TV show with the lowest rating
+  end 
+  
+  
+  def self.least_popular_show
+    # returns the tv show with the lowest rating
+  end 
+  
+  
+  def self.ratings_sum
+    # returns the sum of all the ratings of all the tv shows
+  end 
+  
+  
+  def self.popular_shows
+    # returns an array of all of the shows with a rating above 5
+  end 
+  
+  
+  def self.shows_by_alphabetical_order
+    # returns an array of all of the shows, listed in alphabetical order
+  end 
+    
+    
+    
 end 
